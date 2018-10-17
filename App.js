1506835782f7a8a2b479a6e724628b8e10f87912
htorohn <htorohn@gmail.com>
2018-10-17 07:54:12 -0600

    no necesitamos Icon.ioncons.font porque no usamos tabs

--- App.js
+++ App.js
@@ -51,7 +51,7 @@ export default class App extends React.Component {
       // ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
+        //...Icon.Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
         'space-mono': require('./src/assets/fonts/SpaceMono-Regular.ttf'),
